f9b25b4baea224c1570421cc1af3d919ee3a6056
Magdalena Korgul <magdalenakorgul@MBP-Magdalena.fritz.box>
2024-05-05 23:18:00 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,31 @@ import Navbar from "./components/Navbar";
 import TableHeader from "./components/TableHeader";
 import StudentCard from "./components/StudentCard";
 
+
 import studentsData from "./assets/students.json";
 
 function App() {
   const [students, setStudents] = useState(studentsData);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    image:"",
+    phone:"",
+    email:"",
+    program: "",
+    gratuationYear: "",
+    graduated: {},
+  });
+
+  const handleInputChange = (e) => {
+    const {name, value, type, checked} = e.target;
+    const newValue = type === 'checkbox' ? checked : value;
+    setFormData({...formData, [name]: newValue });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
 
 
   return (
@@ -15,27 +36,31 @@ function App() {
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input name="fullName" type="text" placeholder="Full Name" input-type="" value={formData.fullName}
+              onChange={handleInputChange} />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input name="image" type="url" placeholder="Profile Image" input-type="" value={formData.image}
+              onChange={handleInputChange}/>
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input name="phone" type="tel" placeholder="Phone" input-type="" value={formData.phone}
+              onChange={handleInputChange} />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input name="email" type="email" placeholder="Email" input-type="" value={formData.email}
+              onChange={handleInputChange} />
           </label>
         </div>
 
@@ -43,7 +68,9 @@ function App() {
           <label>
             Program
             <select name="program">
-              <option value="">-- None --</option>
+            value={formData.program}
+              onChange={handleInputChange}
+              <option value="" selected>-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
               <option value="Data">Data</option>
@@ -56,6 +83,9 @@ function App() {
               name="graduationYear"
               type="number"
               placeholder="Graduation Year"
+              value={formData.gratuationYear}
+              onChange={handleInputChange}
+              value={2024}
               minLength={4}
               maxLength={4}
               min={2023}
@@ -65,7 +95,8 @@ function App() {
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input name="graduated" type="checkbox" defaultValue={false} value={formData.graduated}
+              onChange={handleInputChange} />
           </label>
 
           <button type="submit">Add Student</button>
